ad2197e359cbdf9e99a6c875d2c0e79c63865ddc
Adrian <adrianmc@mit.edu>
2025-10-20 15:49:46 -0400

    refactor 3d viz

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Suspense, type FC } from 'react'
 
-import SliderApp from './slider/App'
+import ThreeDVizApp from './3dviz/App'
 import OverlayExperience from './overlay/App'
 
 const App: FC = () => {
@@ -8,7 +8,7 @@ const App: FC = () => {
     <div className='root-shell'>
       <div className='background-layer'>
         <Suspense fallback={<div className='loading-banner'>Loading visualization…</div>}>
-          <SliderApp />
+          <ThreeDVizApp />
         </Suspense>
       </div>
       <div className='overlay-layer'>
